9cc46ced0502967e3b5b16ebfe1af57d08d1ef2a
Ismaël Lachheb <ismael.lachheb@protonmail.com>
2022-06-10 16:27:37 +0200

    clean
    Co-authored-by: Mathieu Tammaro <Maro1F431@users.noreply.github.com>

--- gilded_rose.py
+++ gilded_rose.py
@@ -7,33 +7,21 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            item.update()
+
+
+class ItemFactory:
+
+    @staticmethod
+    def create_item(name: str, quality: int, sell_in: int):
+        if name == "Aged Brie":
+            return AgedBrie(name, sell_in, quality)
+        elif name == "Sulfuras":
+            return Sulfuras(name, sell_in, quality)
+        elif name == "Backstage passes":
+            return BackStagePass(name, sell_in, quality)
+        
+        return Item(name, sell_in, quality)
 
 
 class Item:
@@ -44,3 +32,51 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+    def update(self):
+        if self.quality <= 0:
+            return
+
+        if self.quality < 2 and self.sell_in > 0:
+            self.quality = 0
+            return
+
+        if self.sell_in > 0:
+            self.quality = self.quality - 1
+        else:
+            self.quality = self.quality - 2
+
+
+class AgedBrie(Item):
+
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update(self):
+        if self.quality < 50:
+            self.quality = self.quality + 1
+
+
+class Sulfuras(Item):
+
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update(self):
+        pass
+
+
+class BackStagePass(Item):
+
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update(self):
+        if 5 < self.sell_in <= 10:
+            self.quality = self.quality + 2
+        elif 0 < self.sell_in <= 5:
+            self.quality = self.quality + 3
+        elif self.sell_in <= 0:
+            self.quality = 0
+        else:
+            self.quality = self.quality + 1
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,15 +1,106 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from gilded_rose import Item, GildedRose
+from gilded_rose import GildedRose, ItemFactory
+
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
+
+    def test_quality_degrade_twice_as_fast_when_sell_date_inferior_to_O(self):
+        # given
+        item = ItemFactory.create_item("foo", sell_in=0, quality=4)
+        expected_quality = 2
+        gilded_rose = GildedRose([item])
+
+        # when
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+
+        # then
+        self.assertEquals(item.quality, expected_quality)
+
+    def test_quality_always_superior_to_O(self):
+        # given
+        item = ItemFactory.create_item("foo", sell_in=0, quality=0)
+        gilded_rose = GildedRose([item])
+
+        # when
+        gilded_rose.update_quality()
+
+        # then
+        self.assertGreaterEqual(item.quality, 0)
+
+    def test_aged_bried_increase_quality_as_day_pass(self):
+        # given
+        item = ItemFactory.create_item("Aged Brie", sell_in=3, quality=5)
+        gilded_rose = GildedRose([item])
+
+        # when
+        gilded_rose.update_quality()
+
+        # then
+        assert item.quality == 6
+    
+    def test_item_quality_lower_50(self):
+        # given
+        item = ItemFactory.create_item("Aged Brie", sell_in=3, quality=50)
+        gilded_rose = GildedRose([item])
+
+        # when
+        gilded_rose.update_quality()
+
+        # then
+        self.assertLessEqual(item.quality, 50)
+
+    def test_sulfuras_never_decrease_in_quality(self):
+        # given
+        item = ItemFactory.create_item("Sulfuras", sell_in=1, quality=10)
+        gilded_rose = GildedRose([item])
+
+        # when
+        gilded_rose.update_quality()
+
+        # then
+        assert item.quality == 10
+
+    def test_backstage_quality_increase_by_2_when_sellin_lower_or_equal_10(self):
+        # given
+        item1 = ItemFactory.create_item("Backstage passes", sell_in=10, quality=10)
+        item2 = ItemFactory.create_item("Backstage passes", sell_in=8, quality=10)
+        gilded_rose = GildedRose([item1, item2])
+
+        # when
+        gilded_rose.update_quality()
+
+        # then
+        assert item1.quality == 12
+        assert item2.quality == 12
+
+    def test_backstage_quality_increase_by_3_when_sellin_lower_or_equal_5(self):
+        # given
+        item1 = ItemFactory.create_item("Backstage passes", sell_in=5, quality=10)
+        item2 = ItemFactory.create_item("Backstage passes", sell_in=3, quality=10)
+        gilded_rose = GildedRose([item1, item2])
+
+        # when
+        gilded_rose.update_quality()
+
+        # then
+        assert item1.quality == 13
+        assert item2.quality == 13
+    
+    def test_backstage_quality_is_0_when_sellin_is_passed(self):
+        item1 = ItemFactory.create_item("Backstage passes", sell_in=0, quality=10)
+        item2 = ItemFactory.create_item("Backstage passes", sell_in=-1, quality=10)
+
+        gilded_rose = GildedRose([item1, item2])
+
+        # when
+        gilded_rose.update_quality()
+
+        # then
+        assert item1.quality == 0
+        assert item2.quality == 0
 
 
 if __name__ == '__main__':
